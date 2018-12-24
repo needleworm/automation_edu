429b7d0eea2b7fa9312062f49418e3080c872d39
needleworm <bhban@kakao.com>
2018-12-24 14:58:28 +0900

    first example

---
+++ textmerge/main.py
@@ -0,0 +1,23 @@
+"""
+Author : Byunghyun Ban
+Last Modification : 2018.12.24.
+"""
+
+import os
+
+directory = "personal_info/"
+outfile_name = "merged_personal_info.txt"
+
+out_file = open(outfile_name, 'w')
+
+files = os.listdir(directory)
+
+for filename in files:
+    if ".txt" not in filename:
+        continue
+    file = open(directory + filename)
+    for line in file:
+        out_file.write(line)
+    out_file.write("\n\n")
+    file.close()
+out_file.close()
---
+++ textmerge/sample_generator.py
@@ -0,0 +1,37 @@
+"""
+Author : Byunghyun Ban
+Last Modification : 2018.12.24.
+"""
+import time
+import random
+import os
+
+ALPHABET_SAMPLES = "abcdefghijklmnopqrstuvwxyz"
+
+# number of samples
+NUM_SAMPLES = 2000
+
+# a function to generate random string
+def random_string(length):
+    result = ""
+    for i in range(length):
+        result += random.choice(ALPHABET_SAMPLES)
+    return result
+
+
+# make a directory for sample files
+os.popen("mkdir personal_info")
+time.sleep(2)
+
+
+# generating sample information
+for i in range(NUM_SAMPLES):
+    filename = "personal_info/" + random_string(3) + str(time.time())[-5:] + ".txt"
+    outfile = open(filename, 'w')
+    outfile.write("name : " + random_string(5) + "\n")
+    outfile.write("age : " + str(time.time())[-2:] + "\n")
+    outfile.write("e-mail : " + random_string(8) + "@needle.worm\n")
+    outfile.write("division : " + random_string(3) + "\n")
+    outfile.write("telephone : 010-" + str(time.time())[-4:] + "-" + str(time.time())[-6:-2] + '\n')
+    outfile.write("sex : " + random.choice(["male", "female", "others"]))
+    outfile.close()
